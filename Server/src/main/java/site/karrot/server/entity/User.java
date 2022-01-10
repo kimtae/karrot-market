66a1b10c249526aa71a052333774c84d25763c09
KyeongWoo <kasugano_@naver.com>
2022-01-10 13:25:34 +0900

    Server.Add : #10 유저 프로필 엔티티

--- Server/src/main/java/site/karrot/server/entity/User.java
+++ Server/src/main/java/site/karrot/server/entity/User.java
@@ -1,15 +1,12 @@
 package site.karrot.server.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
---
+++ Server/src/main/java/site/karrot/server/entity/UserProfile.java
@@ -0,0 +1,29 @@
+package site.karrot.server.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    private String profileImgUrl;
+
+    @Column(nullable = false)
+    private String homeAddress;
+
+    private String nearAddress1;
+
+    private String nearAddress2;
+}
