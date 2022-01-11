42de2bf6d11ee943c3384b432497cad1f3f94dfc
KyeongWoo <kasugano_@naver.com>
2022-01-11 13:12:49 +0900

    Server.Modify : #10 로케이션 세분화

--- Server/src/main/java/site/karrot/server/entity/CityLocation.java
+++ Server/src/main/java/site/karrot/server/entity/CityLocation.java
@@ -3,12 +3,12 @@ package site.karrot.server.entity;
 import javax.persistence.*;
 
 @Entity
-public class Location {
+public class CityLocation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private String LocateAddress;
+    private String CityLocation;
 }
