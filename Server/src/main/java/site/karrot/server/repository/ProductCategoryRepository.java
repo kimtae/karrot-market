ab51f2d54177e14ace3531c3c2fbf2911ee71e25
KyeongWoo <kasugano_@naver.com>
2022-01-12 13:23:03 +0900

    Server.Add : 카테고리 리포지토리 인터페이스

--- Server/src/main/java/site/karrot/server/repository/ProductCategoryRepository.java
+++ Server/src/main/java/site/karrot/server/repository/ProductCategoryRepository.java
@@ -1,4 +1,11 @@
 package site.karrot.server.repository;
 
-public interface ProductCategoryRepository{
+import org.springframework.data.repository.Repository;
+import site.karrot.server.entity.ProductCategory;
+
+import java.util.Optional;
+
+public interface ProductCategoryRepository extends Repository<ProductCategory, Long> {
+
+    Optional<ProductCategory> findByProductCategory(String productCategory);
 }
