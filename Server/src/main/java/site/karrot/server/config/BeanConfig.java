7f51aaf18f1e1eeefa7752ed1d01f2daf8f6ee00
KyeongWoo <kasugano_@naver.com>
2022-01-12 12:26:35 +0900

    Server.Add : 서비스와 컨픽에 리포지토리 추가

--- Server/src/main/java/site/karrot/server/config/BeanConfig.java
+++ Server/src/main/java/site/karrot/server/config/BeanConfig.java
@@ -1,14 +1,25 @@
 package site.karrot.server.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import site.karrot.server.repository.ProductRepository;
 import site.karrot.server.service.ProductService;
 
 @Configuration
 public class BeanConfig {
 
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public BeanConfig(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    //프로덕트 서비스 빈 등록
     @Bean
     public ProductService scanProductService() {
-        return new ProductService();
+        return new ProductService(productRepository);
     }
+
 }
--- Server/src/main/java/site/karrot/server/service/ProductService.java
+++ Server/src/main/java/site/karrot/server/service/ProductService.java
@@ -1,10 +1,19 @@
 package site.karrot.server.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import site.karrot.server.dto.ProductDto;
 import site.karrot.server.entity.Product;
 import site.karrot.server.entity.ProductCategory;
+import site.karrot.server.repository.ProductRepository;
 
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     public Product postingProduct(ProductDto.Request request, String category) {
 
