8ed1f8be82fde997867e159be5052a901fe84f3e
KyeongWoo <kasugano_@naver.com>
2022-01-13 12:48:54 +0900

    Server.Add : #23 상품 익셉션 커스텀

--- Server/src/main/java/site/karrot/server/customException/productException/ProductException.java
+++ Server/src/main/java/site/karrot/server/customException/productException/ProductException.java
@@ -1,2 +1,22 @@
-package site.karrot.server.customException.productException;public class ProductException {
+package site.karrot.server.customException.productException;
+
+import lombok.Getter;
+
+
+@Getter
+public class ProductException extends RuntimeException{
+    private ProductErrorCode productErrorCode;
+    private String detailMessage;
+
+    public ProductException(ProductErrorCode productErrorCode){
+        super(productErrorCode.getMessage());
+        this.productErrorCode = productErrorCode;
+        this.detailMessage = productErrorCode.getMessage();
+    }
+
+    public ProductException(ProductErrorCode productErrorCode, String detailMessage) {
+        super(detailMessage);
+        this.productErrorCode = productErrorCode;
+        this.detailMessage = detailMessage;
+    }
 }
